70ead7ef350e1bec0c5e7a4b7492d6cdf06311e8
reyhanneh-gh <reyhaneh80ghalambor@gmail.com>
2026-02-26 03:20:43 +0330

    style and js: side menu

--- src/js/master.js
+++ src/js/master.js
@@ -1,10 +1,30 @@
 // ----------------------slider------------------------
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+// ------------large screen side menu---------------
+let menuBtn = document.getElementById("menu")
+let sideMenu = document.querySelectorAll(".sideMenu")
+let closeBtn = document.querySelectorAll(".closeBtn")
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.forEach(val => {
+    val.style.right = "0"
+    val.style.transition = "0.4s"
+  })
+})
+
+closeBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    sideMenu.forEach(val => {
+      val.style.right = "-100%"
+      val.style.transition = "0.4s"
+    })
+  })
+})
